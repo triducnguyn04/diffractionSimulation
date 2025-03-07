22ebc6a1d5efea1d59c17b5ad9a3d9a89ccd4863
Tri Duc Nguyen <137370596+triducnguyn04@users.noreply.github.com>
2025-03-07 01:14:21 -0500

    Update cifparser.h

--- include/cifparser.h
+++ include/cifparser.h
@@ -5,7 +5,7 @@
 #include <gemmi/unitcell.hpp>
 #include <vector>
 #include <string>
-#include "aligned_memory.h" // Include the new header
+#include "aligned_memory.h"
 
 class CIFParser {
 public:
@@ -19,8 +19,23 @@ public:
                      aligned_vector<std::string>& elements, 
                      gemmi::UnitCell& cell);
 
+    // Static method to load from stored data if available
+    static bool loadStoredData(aligned_vector<float>& x, 
+                               aligned_vector<float>& y, 
+                               aligned_vector<float>& z, 
+                               aligned_vector<std::string>& elements, 
+                               gemmi::UnitCell& cell);
+
+    // Static flag to check if data has been loaded
+    static bool hasLoadedData;
+
 private:
     float to_float(const std::string* str) const;
+
+    // Static storage for previously loaded CIF data
+    static aligned_vector<float> stored_x, stored_y, stored_z;
+    static aligned_vector<std::string> stored_elements;
+    static gemmi::UnitCell stored_cell;
 };
 
 #endif // CIFPARSER_H
